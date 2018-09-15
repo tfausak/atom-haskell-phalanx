8fe083a77d33bfe36f00ae77ef7b65a3d4f448da
Taylor Fausak <taylor@fausak.me>
2018-09-15 12:52:25 -0400

    Fix ESLint warnings

--- index.js
+++ index.js
@@ -2,50 +2,68 @@ const api = require('atom');
 const path = require('path');
 const process = require('child_process');
 
-module.exports = {
-  subscriptions: null,
+const addWarningNotification = (stderr) => atom.notifications.addWarning(
+  'Haskell Phalanx',
+  { detail: stderr.join(''), dismissable: true }
+);
 
-  activate() {
+const addErrorNotification = (stderr, stdout) => atom.notifications.addError(
+  'Haskell Phalanx',
+  { detail: stderr.concat(stdout).join(''), dismissable: true }
+);
+
+const callBrittany = (editor, done) => {
+  const file = editor.getPath();
+  const brittany = process.spawn(
+    'stack',
+    ['exec', '--', 'brittany'],
+    { cwd: file ? path.dirname(file) : null }
+  );
+
+  const stdout = [];
+  const stderr = [];
+  brittany.stdout.on('data', (chunk) => stdout.push(chunk));
+  brittany.stderr.on('data', (chunk) => stderr.push(chunk));
+
+  const stdin = editor.getText();
+  brittany.on('close', (status) => done({ status, stderr, stdin, stdout }));
+
+  brittany.stdin.write(stdin);
+  brittany.stdin.end();
+};
+
+module.exports = {
+  activate () {
     this.subscriptions = new api.CompositeDisposable();
-    this.subscriptions.add(atom.commands.add('atom-workspace',
-      { 'haskell-phalanx:format': () => this.format() }));
+    this.subscriptions.add(atom.commands.add(
+      'atom-workspace',
+      { 'haskell-phalanx:format': () => this.format() }
+    ));
   },
 
-  deactivate() {
+  deactivate () {
     this.subscriptions.dispose();
   },
 
-  format() {
+  format () {
     const editor = atom.workspace.getActiveTextEditor();
-    if (!editor) { return; }
+    if (editor) {
+      callBrittany(editor, ({ status, stderr, stdin, stdout }) => {
+        if (status === 0) {
+          if (stderr.length !== 0) {
+            addWarningNotification(stderr);
+          }
 
-    const file = editor.getPath();
-    const directory = file ? path.dirname(file) : null;
-    const brittany = process.spawn('stack', ['exec', '--', 'brittany'],
-      { cwd: directory });
-
-    const stdout = [];
-    const stderr = [];
-    brittany.stdout.on('data', (chunk) => stdout.push(chunk));
-    brittany.stderr.on('data', (chunk) => stderr.push(chunk));
-
-    const input = editor.getText();
-    brittany.on('close', (status) => {
-      console.log('close', status, stdout, stderr, input)
-      if (status !== 0) {
-        return atom.notifications.addError('Haskell Phalanx',
-          { detail: stderr.concat(stdout).join(''), dismissable: true });
-      }
-      if (stderr.length !== 0) {
-        atom.notifications.addWarning('Haskell Phalanx',
-          { detail: stderr.join(''), dismissable: true });
-      }
-
-      const output = stdout.join('');
-      if (output !== input) { editor.setText(output); }
-    });
-
-    brittany.stdin.write(input);
-    brittany.stdin.end();
+          const output = stdout.join('');
+          if (output !== stdin) {
+            editor.setText(output);
+          }
+        } else {
+          addErrorNotification(stderr, stdout);
+        }
+      });
+    }
   },
+
+  subscriptions: null,
 };
